1c443b8196ad8892e5d596b025a5f26e095645e8
tratchapong <tratchapong@gmail.com>
2025-06-19 12:19:32 +0700

    register api : done

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,37 @@
-import createErrorUtil from "../utils/create-error.util.js"
+import bcrypt from 'bcryptjs'
+import prisma from '../config/prisma.config.js'
+import createError from "../utils/create-error.util.js"
+import checkIdentityKey from "../utils/identity-key.util.js"
 
-export function register(req, res) {
-  req.send('Register Controller')
+
+export async function register(req, res) {
+  const { identity, firstName, lastName, password, confirmPassword } = req.body
+    // validation
+		if (!(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim())) {
+			createError(400, 'Please fill all data')
+		}
+		if (password !== confirmPassword) {
+			createError(400, 'please check confirm-password ')
+		}
+    // identity เป็น email หรือ mobile phone number
+		const identityKey = checkIdentityKey(identity)
+    // หาว่ามี user นี้แล้วหรือยัง
+		const findIdentity = await prisma.user.findUnique({
+			where: { [identityKey]: identity }
+		})
+    if (findIdentity) {
+			createError(409, `Already have this user : ${identity}`)
+		}
+    // เตรียมข้อมูล new user + hash password
+		const newUser = {
+			[identityKey]: identity,
+			password: await bcrypt.hash(password, 10),
+			firstName: firstName,
+			lastName: lastName
+		}
+    // สร้าง new user ใน database 
+		const result = await prisma.user.create({ data: newUser })
+		res.json({ msg: `Register successful`, result })
 }
 
 export function login(req,res) {
