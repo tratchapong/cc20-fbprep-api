58bb9a8b628a30bf23d50e139a73579c4f5c7ed7
tratchapong <tratchapong@gmail.com>
2025-06-21 16:31:48 +0700

    improve validator : registerSchema

--- src/validations/validator.js
+++ src/validations/validator.js
@@ -14,27 +14,24 @@ export const registerSchema = object({
         return emailRegex.test(value) || mobileRegex.test(value)
       }),
   password: string().min(4).required(),
-  confirmPassword: string().oneOf([ref("password")], `confirmPassword must match password`)
-}).transform((value) => {
-  const { identity, confirmPassword, ...rest } = value;
-  let result = emailRegex.test(identity) ? { email: identity } : { mobile: identity }
-  return { ...rest, ...result, identity }
+  confirmPassword: string().oneOf([ref("password")], `confirmPassword must match password`),
+  email: string().email(),
+  mobile: string().matches(mobileRegex)
+}).noUnknown()
+.transform((value) => {
+  return ({ ...value, [emailRegex.test(value.identity) ? 'email' : 'mobile'] : value.identity })
 })
 
 export function validate(schema, options = {}) {
   return async function (req, res, next) {
     try {
-      console.log('validate run...')
-      const result = await schema.validate(req.body, { abortEarly: false })
-      req.body = { ...result }
-      if (options.removeIdentity) {
-        delete req.body.identity
-      }
+      const cleanBody = await schema.validate(req.body,
+         { abortEarly: false, ...options })
+      req.body = cleanBody
       next()
     } catch (err) {
       let errMsg = err.errors.join('|||')
       console.log(errMsg)
-      // next(errMsg)
       createError(400,errMsg)
     }
   }
--- src/validations/yup-test.js
+++ src/validations/yup-test.js
@@ -1,28 +1,32 @@
 import { object, string, number, date } from 'yup';
 
-let userSchema = object({
-  name: string('fill name please').required(),
-  age: number().required().positive('อายุเป็นเลขบวกดิ').integer(),
-  email: string().email(),
-  website: string().url().nullable(),
-  createdOn: date().default(() => new Date()),
-});
+// let userSchema = object({
+//   name: string('fill name please').required(),
+//   age: number().required().positive('อายุเป็นเลขบวกดิ').integer(),
+//   email: string().email(),
+//   website: string().url().nullable(),
+//   createdOn: date().default(() => new Date()),
+// });
 
-let data = {
-  name : '',
-  age: 0.9,
-  email : 'n@true'
-}
+// let data = {
+//   name : '',
+//   age: 0.9,
+//   email : 'n@true'
+// }
 
-userSchema.validate(data, {abortEarly: false}).then(rs=>{
-  console.log('Result :', rs)
-}).catch(err => {
-  console.log('*** ERROR ***')
-  console.log(err.errors.join('\n'))
-  // console.log(err.errors.toString())
-  // console.log('*************')
-  // console.log(err.inner)
-  for(let e of err.inner) {
-    console.log(`Error item => ${e.path} : ${e.errors[0]}`)
-  }
-})
+// userSchema.validate(data, {abortEarly: false}).then(rs=>{
+//   console.log('Result :', rs)
+// }).catch(err => {
+//   console.log('*** ERROR ***')
+//   console.log(err.errors.join('\n'))
+//   // console.log(err.errors.toString())
+//   // console.log('*************')
+//   // console.log(err.inner)
+//   for(let e of err.inner) {
+//     console.log(`Error item => ${e.path} : ${e.errors[0]}`)
+//   }
+// })
+
+// console.log(string())
+console.log(string().email().isValidSync("andy@codecamp"))
+// console.log(string())
