b529ab53a5a53ddddcff18db5ad97c0b46819cb5
tratchapong <tratchapong@gmail.com>
2025-06-23 12:56:08 +0700

    start user.service.js + refactor auth.controller.js

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,109 +3,91 @@ import jwt from 'jsonwebtoken'
 import prisma from '../config/prisma.config.js'
 import createError from "../utils/create-error.util.js"
 import checkIdentityKey from "../utils/identity-key.util.js"
+import { createUser, getUserBy } from '../services/user.service.js'
 
 
 export async function register(req, res, next) {
-  try {
-    const { identity, firstName, lastName, password, confirmPassword } = req.body
-    // validation
-    if (!(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim())) {
-      createError(400, 'Please fill all data')
-    }
-    if (password !== confirmPassword) {
-      createError(400, 'please check confirm-password ')
-    }
-    // identity เป็น email หรือ mobile phone number
-    const identityKey = checkIdentityKey(identity)
-    // หาว่ามี user นี้แล้วหรือยัง
-    const findIdentity = await prisma.user.findUnique({
-      where: { [identityKey]: identity }
-    })
-    if (findIdentity) {
-      createError(409, `Already have this user : ${identity}`)
-    }
-    // เตรียมข้อมูล new user + hash password
-    const newUser = {
-      [identityKey]: identity,
-      password: await bcrypt.hash(password, 10),
-      firstName: firstName,
-      lastName: lastName
-    }
-    // สร้าง new user ใน database 
-    const result = await prisma.user.create({ data: newUser })
-    res.json({ msg: `Register successful`, result })
-  } catch (err) {
-    next(err)
+  const { identity, firstName, lastName, password, confirmPassword } = req.body
+  // validation
+  if (!(identity.trim() && firstName.trim() && lastName.trim() && password.trim() && confirmPassword.trim())) {
+    createError(400, 'Please fill all data')
   }
+  if (password !== confirmPassword) {
+    createError(400, 'please check confirm-password ')
+  }
+  // identity เป็น email หรือ mobile phone number
+  const identityKey = checkIdentityKey(identity)
+  // หาว่ามี user นี้แล้วหรือยัง
+  const findIdentity = await getUserBy(identityKey, identity)
+  if (findIdentity) {
+    createError(409, `Already have this user : ${identity}`)
+  }
+  // เตรียมข้อมูล new user + hash password
+  const newUser = {
+    [identityKey]: identity,
+    password: await bcrypt.hash(password, 10),
+    firstName: firstName,
+    lastName: lastName
+  }
+  // สร้าง new user ใน database 
+  const result = await createUser(newUser)
+  res.json({ msg: `Register successful`, result })
 }
 export async function registerYup(req, res, next) {
-    // console.log(req.body)
-    const { email, mobile, firstName, lastName, password } = req.body
-    // หาว่ามี user นี้แล้วหรือยัง
-    if (email) {
-      let foundUserEmail = await prisma.user.findUnique({
-        where: { email: email }
-      })
-      if (foundUserEmail) {
-        createError(409, `Email: ${foundUserEmail.email} have already registered`)
-      }
-    }
-    if (mobile) {
-      let foundUserMobile = await prisma.user.findUnique({
-        where: { mobile: mobile }
-      })
-      if (foundUserMobile) {
-        createError(409, `Mobile: ${foundUserMobile.mobile} have already registered`)
-      }
-    }
-    // เตรียมข้อมูล new user + hash password
-    const newUser = {
-      email,
-      mobile,
-      password: await bcrypt.hash(password, 10),
-      firstName: firstName,
-      lastName: lastName
-    }
-    // สร้าง new user ใน database 
-    const result = await prisma.user.create({ data: newUser })
-    res.json({ msg: `Register successful`, result })
-    // res.json({ msg: `Register successful`, newUser })
+  // console.log(req.body)
+  const { email, mobile, firstName, lastName, password } = req.body
+  // ถ้ามี user แล้วให้แจ้ง eroor 
+  if (email && await getUserBy('email', email)) {
+    createError(409, `Email: ${email} have already registered`)
   }
+  if (mobile && await getUserBy('mobile', mobile)) {
+    createError(409, `Mobile: ${mobile} have already registered`)
+  }
+  // เตรียมข้อมูล new user + hash password
+  const newUser = {
+    email,
+    mobile,
+    password: await bcrypt.hash(password, 10),
+    firstName: firstName,
+    lastName: lastName
+  }
+  // สร้าง new user ใน database 
+  const result = await createUser(newUser)
+  res.json({ msg: `Register successful`, result })
+}
 
 export async function login(req, res) {
-    // console.log(req.body)
-    const { identity, password, email, mobile } = req.body
-    const identityKey = email ? 'email' : 'mobile'
-    // console.log(identityKey)
+  // console.log(req.body)
+  const { identity, password, email, mobile } = req.body
+  const identityKey = email ? 'email' : 'mobile'
+  // console.log(identityKey)
 
-    // find user
-    const foundUser = await prisma.user.findUnique({
-      where: { [identityKey]: identity }
-    })
+  // find user
+  const foundUser = await getUserBy(identityKey, identity)
 
-    if (!foundUser) {
-      createError(401, 'Invalid Login')
-    }
-    // check password
-    let pwOk = await bcrypt.compare(password, foundUser.password)
-    if (!pwOk) {
-      createError(401, 'Invalid Login')
-    }
-
-    // create jwt token
-
-    const payload = { id: foundUser.id }
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '15d'
-    })
-    const { password: pw, createdAt, updatedAt, ...userData } = foundUser
-
-    res.json({
-      msg: 'Login Controller',
-      token: token,
-      user: userData
-    })
+  if (!foundUser) {
+    createError(401, 'Invalid Login')
   }
+  // check password
+  let pwOk = await bcrypt.compare(password, foundUser.password)
+  if (!pwOk) {
+    createError(401, 'Invalid Login')
+  }
+
+  // create jwt token
+
+  const payload = { id: foundUser.id }
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: '15d'
+  })
+  const { password: pw, createdAt, updatedAt, ...userData } = foundUser
+
+  res.json({
+    msg: 'Login Controller',
+    token: token,
+    user: userData
+  })
+}
 
 export const getMe = (req, res) => {
   res.json({ msg: 'GetMe controller' })
